3695345f2f7225a93eae826ad95db9303f55db41
MysticMak100 <ateequekhan101@gmail.com>
2024-08-28 18:11:40 +0530

    Changes

--- back/index.js
+++ back/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongodb from "./mongodb.js";
@@ -33,10 +32,12 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Your frontend URL
+    origin: "https://main--teacher-student-db.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
 
